05323f4833143a768604c8524bac6158bf4f1e81
DanRamage <dan@inlet.geol.sc.edu>
2016-05-20 15:01:52 -0400

    Init checkin for the plugin.

--- scripts/mb_prediction_plugin.py
+++ scripts/mb_prediction_plugin.py
@@ -4,6 +4,8 @@ sys.path.append('../../commonfiles/python')
 sys.path.append(dirname(realpath(__file__)))
 from datetime import datetime
 from pytz import timezone
+import logging.config
+import ConfigParser
 
 from wq_prediction_plugin import wq_prediction_engine_plugin
 from mb_wq_prediction_engine import run_wq_models
@@ -14,10 +16,18 @@ class mb_prediction_plugin(wq_prediction_engine_plugin):
     self.logger.debug("inititalize_plugin Started")
     self.config_file = kwargs['ini']
     self.process_dates = kwargs.get('process_date', None)
+    self.name = kwargs['name']
     self.logger.debug("inititalize_plugin Finished")
 
-  def run_wq_models(self, **kwargs):
-    self.logger.debug("run_wq_models Started")
+  #def run_wq_models(self, **kwargs):
+  def run(self):
+    config_file = ConfigParser.RawConfigParser()
+    config_file.read(self.config_file)
+    logConfFile = config_file.get('logging', 'prediction_engine')
+
+    logging.config.fileConfig(logConfFile)
+    logger = logging.getLogger("mb_logger")
+    logger.debug("run_wq_models Started. Process: %s" % (self.name))
     dates_to_process = []
     if self.process_dates is not None:
       #Can be multiple dates, so let's split on ','
@@ -47,5 +57,5 @@ class mb_prediction_plugin(wq_prediction_engine_plugin):
         run_wq_models(begin_date=process_date,
                       config_file_name=self.config_file)
     except Exception, e:
-      self.logger.exception(e)
-    self.logger.debug("run_wq_models Finished")
+      logger.exception(e)
+    logger.debug("run_wq_models Finished")
