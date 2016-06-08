c03ebc31e1022289a15055b10d4fbf2d4002166c
DanRamage <dan@inlet.geol.sc.edu>
2016-06-08 11:12:35 -0400

    Fix up s order issues.

--- scripts/mb_import_pier_historical_data.sh
+++ scripts/mb_import_pier_historical_data.sh
@@ -4,4 +4,4 @@ source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
 
 cd /home/xeniaprod/scripts/MyrtleBeach-Water-Quality/scripts;
 
-python build_historical_database.py --ConfigFile=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/config/mb_historic_data_config.ini --DataFile="/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/data/historic/2nd Ave Pier 060414 to 123114.csv" --Header="Date,Time,Depth,Temp,Salinity,ODO Conc,ODO%,pH,Chlorophyll,Turbidity+,Depth,Temp,Salinity,ODO Conc,ODO%,pH,Chlorophyll,Turbidity+,Air Temp,BP,RH,Wind Dir,Wind Speed,Rainfall" --FirstDataRow=2 --PlatformName=lbhmc.2ndAveNorth.pier
+python build_historical_database.py --ConfigFile=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/config/mb_historic_data_config.ini --DataFile="/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/data/historic/2nd Ave Pier 060414 to 123114.csv" --Header="Date,Time,Depth,Temp,Salinity,ODO Conc,ODO%,pH,Chlorophyll,Turbidity+,Depth,Temp,Salinity,ODO Conc,ODO%,pH,Chlorophyll,Turbidity+,Air Temp,BP,RH,Wind Dir,Wind Speed,Rainfall" --FirstDataRow=11 --PlatformName=lbhmc.2ndAveNorth.pier
