e5aa6033e24e2eeb7c41a78968b10f1cc2b2f72e
DanRamage <ChiefDan@gmail.com>
2018-01-09 14:31:00 -0500

    Made modifications to use the new plugin logging.

--- scripts/data_collector_plugin/dhec_sample_data_collector_plugin.py
+++ scripts/data_collector_plugin/dhec_sample_data_collector_plugin.py
@@ -8,15 +8,17 @@ import geojson
 
 from dhecBeachAdvisoryReader import waterQualityAdvisory
 
-class nexrad_collector_plugin(data_collector_plugin):
+class dhec_sample_data_collector_plugin(data_collector_plugin):
 
   def initialize_plugin(self, **kwargs):
+    data_collector_plugin.initialize_plugin(self, **kwargs)
     try:
+      logger = logging.getLogger(self.__class__.__name__)
       plugin_details = kwargs['details']
       self.ini_file = plugin_details.get('Settings', 'ini_file')
       return True
     except Exception as e:
-      self.logger.exception(e)
+      logger.exception(e)
     return False
 
   def run(self):
@@ -24,12 +26,19 @@ class nexrad_collector_plugin(data_collector_plugin):
       configFile = ConfigParser.RawConfigParser()
       configFile.read(self.ini_file)
 
+      self.logging_client_cfg['disable_existing_loggers'] = True
+      logging.config.dictConfig(self.logging_client_cfg)
+      logger = logging.getLogger(self.__class__.__name__)
+      logger.debug("run started.")
+
+      """
       logger = None
       logConfFile = configFile.get('logging', 'scraperConfigFile')
       if(logConfFile):
         logging.config.fileConfig(logConfFile)
         logger = logging.getLogger("dhec_beach_advisory_app")
         logger.info("Log file opened.")
+      """
     except ConfigParser.Error, e:
       print("No log configuration file given, logging disabled.")
     except Exception,e:
